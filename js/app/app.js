f6d2dd2e67abb7355026da873efdfecabf7be704
Umutari <umutariy@gmail.com>
2017-10-20 16:17:10 +0000

    geolocation

---
+++ js/app/app.js
@@ -0,0 +1,32 @@
+// Since we will be making use of 3rd party functions (eg. navigator.geolocation.getCurrentPosition) which are not native javascript functions we will need
+// to add this function to the list of native javascript functions to allow javascript identify and execute it each time its called.
+// This is done by using the addEventListener() function.
+//
+document.addEventListener("deviceready", onDeviceReady, false);
+
+
+//We decide to create a function to handle the 3rd party functions (eg. navigator.geolocation.getCurrentPosition)
+// which we earlier added to the native functions of the javascript
+function onDeviceReady() {
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
+
+// onSuccess Geolocation
+//
+function onSuccess(position) {
+
+    // var element = document.getElementById('geolocation');
+    longitude = position.coords.longitude ;
+    latitude = position.coords.latitude;
+    var coordinates = { lat: latitude, lng: longitude};
+    map.setCenter(coordinates);
+    marker.setPosition(coordinates);
+    
+}
+
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+        'message: ' + error.message + '\n');
+}
